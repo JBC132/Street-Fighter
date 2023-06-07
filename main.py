6a173b69f869634c47ebb099e9b9ca04ca08c206
Jinyoung Rhee <jakerhee321@gmail.com>
2023-06-07 12:20:21 +0900

    Background

--- main.py
+++ main.py
@@ -7,10 +7,21 @@ SCREEN_HEIGHT = 600
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 pygame.display.set_caption("Brawler")
 
+bg_image = pygame.image.load("assets/images/background/background.jpg").convert_alpha()
+
+def draw_bg():
+    scaled_bg = pygame.transform.scale(bg_image, (SCREEN_WIDTH, SCREEN_HEIGHT))
+    screen.blit(bg_image, (0,0))
+
+
 run = True
 while run:
+
+    draw_bg()
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
 
+    pygame.display.update()
 pygame.quit()
