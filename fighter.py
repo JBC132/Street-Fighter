8d815ebd12228c0f6d70e6445fa0405e81ae1d56
Jinyoung Rhee <jakerhee321@gmail.com>
2023-06-10 01:43:27 +0900

    Draw rect

--- fighter.py
+++ fighter.py
@@ -1,4 +1,8 @@
 import pygame
 
 class Fighter():
-    pass
+    def __init__(self, x, y):
+        self.rect = pygame.Rect((x,y,80,180))
+    
+    def draw(self, surface):
+        pygame.draw.rect(surface, (255,0,0), self.rect)
