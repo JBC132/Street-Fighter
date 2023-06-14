96fb63d4bbce9c74a5cd0a5f389bda573825a7df
Jinyoung Rhee <jakerhee321@gmail.com>
2023-06-14 14:53:35 +0900

    Two types of attack

--- fighter.py
+++ fighter.py
@@ -5,7 +5,8 @@ class Fighter():
         self.rect = pygame.Rect((x,y,80,180))
         self.vel_y = 0
         self.jump = False
-    
+        self.attack_type = 0
+
     def move(self, screen_width, screen_height):
         SPEED = 10
         GRAVITY = 2
@@ -22,6 +23,14 @@ class Fighter():
             self.vel_y = -30
             self.jump = True
         
+        if key[pygame.K_r] or key[pygame.K_t]:
+            if key[pygame.K_r]:
+                self.attack_type = 1
+            if key[pygame.K_t]:
+                self.attack_type = 2
+                    
+
+
         self.vel_y += GRAVITY
         dy += self.vel_y
         
@@ -36,6 +45,9 @@ class Fighter():
 
         self.rect.x += dx
         self.rect.y += dy
+
+    def attack(self):
+        pass
 
     def draw(self, surface):
         pygame.draw.rect(surface, (255,0,0), self.rect)
